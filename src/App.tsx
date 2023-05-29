61ebb922eaf048f2f180d20ec037d56faad1ea4f
vivekp2412 <vivekp2412@gmail.com>
2023-05-29 10:09:14 +0530

    Feature HomeSection Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import HomeContainer from "./Containers/HomeContainer/HomeContainer";
 import NavbarContainer from "./Containers/navbarContainer/NavbarContainer";
 function App() {
   return (
     <>
       <NavbarContainer />
+      <HomeContainer />
     </>
   );
 }
