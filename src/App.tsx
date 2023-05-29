5adfeae17ef1377debd5d2dc72a590f322aea1b9
vivekp2412 <vivekp2412@gmail.com>
2023-05-29 10:11:23 +0530

    Feature ProductSection Continue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import HomeContainer from "./Containers/HomeContainer/HomeContainer";
+import ProductContainer from "../src/Containers/ProductContainer/ProductContainer";
 import NavbarContainer from "./Containers/navbarContainer/NavbarContainer";
 function App() {
   return (
     <>
       <NavbarContainer />
-      <HomeContainer />
+      <HomeContainer/>
+      <ProductContainer/>
     </>
   );
 }
